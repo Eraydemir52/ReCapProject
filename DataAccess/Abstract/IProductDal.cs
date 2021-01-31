e260fb140119705793d5393f0683e1ad8e4314fa
Eray Demir <2200656036@nku.edu.tr>
2021-01-31 23:21:57 +0300

    Add project files.

---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    interface IProductDal
+    {
+        List<Product> GetAll();
+
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(Product product);
+
+        void GetById(Product product);
+
+        void GetAll(Product product);
+
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,67 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            _products = new List<Product>
+            {
+                new Product{Id=1,BrandId=1,ColorId=1,DailyPrice=100000,Description="Bmw",ModelYear=2011},
+                new Product{Id=2,BrandId=2,ColorId=2,DailyPrice=200000,Description="Audi",ModelYear=2015},
+                new Product{Id=3,BrandId=3,ColorId=3,DailyPrice=300000,Description="Skoda",ModelYear=2016},
+                new Product{Id=4,BrandId=4,ColorId=4,DailyPrice=400000,Description="Opel",ModelYear=2019}
+
+            };
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            Product productTODelete = _products.SingleOrDefault(X => X.Id==product.Id);
+            _products.Remove(productTODelete);
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public List<Product> GetAll(Product product)
+        {
+            return _products.Where(p => p.Id == product.Id).ToList();
+        }
+
+        
+
+        public void GetById(Product product)
+        {
+           
+        }
+
+        public void Update(Product product)
+        {
+            Product productToUpdate = _products.SingleOrDefault(Y => Y.Id == product.Id);
+            productToUpdate.Id = product.Id;
+            productToUpdate.ColorId = product.ColorId;
+            productToUpdate.DailyPrice = product.DailyPrice;
+            productToUpdate.Description = product.Description;
+            productToUpdate.ModelYear = product.ModelYear;
+        }
+
+        void IProductDal.GetAll(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
